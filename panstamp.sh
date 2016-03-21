bd0e975e733b7c1e262952159161aba9d48928ed
dberenguer <dberenguer@usapiens.com>
2016-03-21 18:41:51 +0100

    New init.d script

--- panstamp.sh
+++ panstamp.sh
@@ -1,15 +1,21 @@
 #! /bin/sh
-#
-# panStamp init.d script for Raspbian
-#
+### BEGIN INIT INFO
+# Provides:          panstamp
+# Required-Start:
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Run lagarto and node-red
+# Description:
+### END INIT INFO
+
+LAGARTO_SWAP=/home/debian/python_tools/lagarto/lagarto-swap/lagarto-swap.py
+#LAGARTO_SWAP=/home/pi/python/lagarto/lagarto-swap/lagarto-swap.py
 
 case "$1" in
   start)
-  echo "Starting lagarto-max"
-  python /home/pi/python/lagarto/lagarto-max/lagarto-max.py &
-  sleep 5
   echo "Starting lagarto-swap"
-  python /home/pi/python/lagarto/lagarto-swap/lagarto-swap.py &
+  python $LAGARTO_SWAP &
   ;;
   stop)
   echo "Killing all python processes"
@@ -19,4 +25,3 @@ esac
 
 exit 0
 
-  
